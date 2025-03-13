88282207b8c79c79121a753232c1e23cc6c47efb
Anay Saha <anaysahapaban@gmail.com>
2025-03-14 01:23:55 +0600

    lorem

--- src/Components/emni.jsx
+++ src/Components/emni.jsx
@@ -4,6 +4,8 @@ const emni = () => {
     return (
         <div>
             <h1>emni</h1>
+            <h1>jnvgfngn</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquam dolores veritatis voluptates, quam libero debitis aperiam ipsum voluptas rem eveniet, obcaecati in! Nisi asperiores consequuntur, dolor eveniet incidunt saepe.</p>
         </div>
     );
 };
