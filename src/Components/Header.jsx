84b7a10e58f41969b12ed1b49f1f2b6d72c5684c
Anay Saha <anaysahapaban@gmail.com>
2025-03-16 03:26:13 +0600

    push

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 const Header = () => {
     return (
         <div>
-            
+           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas, illo expedita accusamus dignissimos deleniti dolores quo aut impedit cumque, itaque nisi doloremque, cupiditate voluptate! Ipsam error dolorem inventore aut similique.
+           <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ipsa ducimus corrupti molestias quidem error.</h1> 
         </div>
     );
 };
